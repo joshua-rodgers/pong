e9840b899f5819fdad4147cb1f4cfe35b2fb1cee
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2021-04-04 15:30:35 -0500

    double buffering implemented

--- src/com/joshuarodgers/Screen.java
+++ src/com/joshuarodgers/Screen.java
@@ -1,27 +1,31 @@
 package com.joshuarodgers;
 
-import java.awt.Panel;
+
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Font;
 
 public class Screen {
-    Panel game_surface;
+    
     Graphics ctx;
+    int width;
+    int height;
     Color message_color;
     Font message_font;
 
 
 
-    public Screen(Panel game_surface, Graphics ctx){
-        this.game_surface = game_surface;
+    public Screen(int width, int height, Graphics ctx){
+        this.width = width;
+        this.height = height;
         this.ctx = ctx;
         message_color = Color.GREEN;
-        message_font = new Font("SANS_SERIF", 0, 48);
+        message_font = new Font("SANS_SERIF", 0, 36);
     }
 
     public void display(String message){
         ctx.setColor(message_color);
-        ctx.drawString(message, game_surface.getWidth() / (message.length() / 2), game_surface.getHeight() / 2);
+        ctx.setFont(message_font);
+        ctx.drawString(message, width / (message.length() / 2), height / 2);
     }
 }
